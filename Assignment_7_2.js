3d7afbdbd5b2091d97ae67fdf6594884912221ee
Kyle Schutter <kmosswork@gmail.com>
2018-05-12 22:05:24 -0600

    Initialized and completed assignment

---
+++ Assignment_7_2.js
@@ -0,0 +1,12 @@
+// For loop
+console.log("Using for loop");
+for (var i = 0; i < 10; i++) {
+    console.log(i*5 + 5);
+}
+
+// While loop
+console.log("Using while loop");
+var j = 0;
+while (j < 50) {
+    console.log(j += 5);
+}
